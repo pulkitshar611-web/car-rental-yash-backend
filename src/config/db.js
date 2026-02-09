f9b69ad026036f71b1dda22db2eba73bd5aa4ef1
GautamBairagi <gauatambairagi22@gmail.com>
2026-02-09 12:16:13 +0530

    dadsa

--- src/config/db.js
+++ src/config/db.js
@@ -5,6 +5,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
+  port: process.env.DB_PORT, 
   database: process.env.DB_NAME || 'car',
   waitForConnections: true,
   connectionLimit: 10,
